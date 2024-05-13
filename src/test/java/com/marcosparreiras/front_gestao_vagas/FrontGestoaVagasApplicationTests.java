3d58a40a563185ee123fcfa34b1a21e402fe1eff
Marcos Parreiras <marcosalp1998@gmail.com>
2024-05-13 10:43:14 -0300

    init: start a spring-boot project with maven and add spirng-boot-web, spring-boot-devtools and thymeleaf dependecies

---
+++ src/test/java/com/marcosparreiras/front_gestao_vagas/FrontGestoaVagasApplicationTests.java
@@ -0,0 +1,11 @@
+package com.marcosparreiras.front_gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontGestaoVagasApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
