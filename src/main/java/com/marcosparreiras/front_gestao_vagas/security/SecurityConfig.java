894d9c8af0b4beaa110eabeea315bf2d75327806
Marcos Parreiras <marcosalp1998@gmail.com>
2024-05-14 12:56:31 -0300

    feat: add security-config to manage authorization wiht spring-boot-security

---
+++ src/main/java/com/marcosparreiras/front_gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.marcosparreiras.front_gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    throws Exception {
+    http
+      .authorizeHttpRequests(auth -> {
+        auth.requestMatchers("/styles/**").permitAll();
+        auth.requestMatchers("/candidate/login").permitAll();
+        auth.requestMatchers("/candidate/sing-in").permitAll();
+        auth.anyRequest().authenticated();
+      })
+      .formLogin(form -> {
+        form.loginPage("/candidate/login");
+      });
+
+    return http.build();
+  }
+}
