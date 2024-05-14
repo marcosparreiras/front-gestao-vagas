7e5b509b5b6026ade610848c1a099d61de627cce
Marcos Parreiras <marcosalp1998@gmail.com>
2024-05-14 13:30:48 -0300

    feat: add spring-security authentication and create session logic on login of candidate controller

--- src/main/java/com/marcosparreiras/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/marcosparreiras/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,7 +1,15 @@
 package com.marcosparreiras.front_gestao_vagas.modules.candidate.controller;
 
+import com.marcosparreiras.front_gestao_vagas.modules.candidate.dto.Token;
 import com.marcosparreiras.front_gestao_vagas.modules.candidate.service.CandidateService;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,11 +31,30 @@ public class CandidateController {
   @PostMapping("/sing-in")
   public String singIn(
     RedirectAttributes redirectAttributes,
+    HttpSession session,
     String username,
     String password
   ) {
     try {
-      this.candidateService.login(username, password);
+      Token token = this.candidateService.login(username, password);
+
+      List<SimpleGrantedAuthority> grants = token
+        .getRoles()
+        .stream()
+        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+        .toList();
+
+      UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+        null,
+        null,
+        grants
+      );
+
+      SecurityContext securityContext = SecurityContextHolder.getContext();
+      securityContext.setAuthentication(auth);
+      session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+      session.setAttribute("TOKEN", token);
+
       return "redirect:/candidate/profile";
     } catch (Exception e) {
       redirectAttributes.addFlashAttribute(
@@ -39,6 +66,7 @@ public class CandidateController {
   }
 
   @GetMapping("/profile")
+  @PreAuthorize("hasRole('CANDIDATE')")
   public String profile() {
     return "/candidate/profile";
   }
--- src/main/java/com/marcosparreiras/front_gestao_vagas/modules/candidate/dto/Token.java
+++ src/main/java/com/marcosparreiras/front_gestao_vagas/modules/candidate/dto/Token.java
@@ -1,8 +1,11 @@
 package com.marcosparreiras.front_gestao_vagas.modules.candidate.dto;
 
+import java.util.List;
+
 public class Token {
 
   private String token;
+  private List<String> roles;
 
   public String getToken() {
     return this.token;
@@ -10,5 +13,13 @@ public class Token {
 
   public void SetToken(String token) {
     this.token = token;
+  }
+
+  public List<String> getRoles() {
+    return this.roles;
+  }
+
+  public void SetToken(List<String> roles) {
+    this.roles = roles;
   }
 }
